5ee8ef67abd7640197eebe63f0c5121da9fa4c56
walther <waltheroliveirapires@gmail.com>
2024-05-29 15:58:04 -0300

    Correção na adição de clientes

--- src/GerenciadorClientes.java
+++ src/GerenciadorClientes.java
@@ -47,6 +47,7 @@ public class GerenciadorClientes implements PesquisaCliente {
         System.out.println("Escolha o tipo de Cliente: 1. Pessoa Física | 2. Cliente VIP");
         int tipoCliente = prompt.nextInt();
         prompt.nextLine();
+
         Cliente cliente = null;
         switch(tipoCliente){
             case 1:
@@ -55,53 +56,72 @@ public class GerenciadorClientes implements PesquisaCliente {
                 System.out.println("Sobrenome: ");
                 String sobrenomePF = prompt.nextLine();
                 String nomeCompletoPF = nomePF + " " + sobrenomePF;
-                for(Cliente aux : clientes){
-                    if(aux.getNome().equalsIgnoreCase(nomeCompletoPF)){
-                        System.out.println("Cliente já cadastrado!");
-                    }
-                    else {
-                        System.out.println("CPF: ");
-                        String cpfPF = prompt.nextLine();
-                        for(Cliente auxCpf : clientes){
-                            if(auxCpf.getCpf().equalsIgnoreCase(cpfPF)){
-                                System.out.println("CPF já cadastrado!");
-                            }
-                            else {
-                                cliente = new PessoaFisica(nomeCompletoPF, cpfPF);
-                                clientes.add(cliente);
-                            }
-                        }
-                    }
+
+                if(clienteCadastrado(nomeCompletoPF)) {
+                    System.out.println("Cliente já cadastrado!");
+                    break;
                 }
+
+                System.out.println("CPF: ");
+                String cpfPF = prompt.nextLine();
+
+                if(cpfCadastrado(cpfPF)){
+                    System.out.println("CPF já cadastrado!");
+                    break;
+                }
+
+                cliente = new PessoaFisica(nomeCompletoPF, cpfPF);
                 break;
+
             case 2:
                 System.out.println("Nome: ");
                 String nomeVIP = prompt.nextLine();
                 System.out.println("Sobrenome: ");
                 String sobrenomeVIP = prompt.nextLine();
                 String nomeCompletoVIP = nomeVIP + " " + sobrenomeVIP;
-                for(Cliente aux : clientes){
-                    if(aux.getNome().equalsIgnoreCase(nomeCompletoVIP)){
-                        System.out.println("Cliente já cadastrado!");
-                    }
-                    else {
-                        System.out.println("CPF: ");
-                        String cpfVIP = prompt.nextLine();
-                        for(Cliente auxCpfVIP : clientes){
-                            if(auxCpfVIP.getCpf().equalsIgnoreCase(cpfVIP)){
-                                System.out.println("CPF já cadastrado!");
-                            }
-                            else {
-                                cliente = new ClienteVIP(nomeCompletoVIP, cpfVIP);
-                                clientes.add(cliente);
-                            }
-                        }
-                    }
+
+                if (clienteCadastrado(nomeCompletoVIP)) {
+                    System.out.println("Cliente VIP já cadastrado!");
+                    break;
                 }
+
+                System.out.println("CPF: ");
+                String cpfVIP = prompt.nextLine();
+
+                if(cpfCadastrado(cpfVIP)){
+                    System.out.println("CPF já cadastrado!");
+                    break;
+                }
+
+                cliente = new ClienteVIP(nomeCompletoVIP, cpfVIP);
                 break;
             default:
                 System.out.println("Opção inválida! Escolha uma opção válida!");
+                break;
         }
+
+        if(cliente != null) {
+            clientes.add(cliente);
+            System.out.println("CLiente cadastrado com sucesso: " + cliente.getNome());
+        }
+    }
+
+    private boolean clienteCadastrado(String nome){
+        for(Cliente cliente : clientes) {
+            if(cliente.getNome().equalsIgnoreCase(nome)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean cpfCadastrado(String cpf){
+        for(Cliente cliente : clientes){
+            if(cliente.getCpf().equalsIgnoreCase(cpf)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public void removerCliente() {
